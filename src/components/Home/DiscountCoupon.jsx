b7f8aa0f0da846a49217abc959207ef9d1f78d9e
sgayatri1982-star <sgayatri.1982@gmail.com>
2025-12-06 12:37:30 +0530

    Updated DiscountCoupon.jsx

--- src/components/Home/DiscountCoupon.jsx
+++ src/components/Home/DiscountCoupon.jsx
@@ -100,7 +100,7 @@ const DiscountCoupon = () => {
                   </div>
                   <div className="col-md-6">
                     <div className="coupon-code bg-white bg-opacity-20 rounded p-3 mb-3">
-                      <div className="d-flex align-items-center justify-content-center">
+                      <div className="d-flex text-grey align-items-center justify-content-center">
                         <Tag size={20} className="me-2" />
                         <strong>{coupon.coupon_code}</strong>
                       </div>
